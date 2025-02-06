9898bd097cadc88a454b7ae5e3caf2f0bdda923b
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-06 15:00:31 +0100

    Add Console 2D and 3D Array

---
+++ Console 2D and 3D Array/Console 2D and 3D Array/Program.cs
@@ -0,0 +1,66 @@
+﻿int[,] array2D = new int[3, 3];
+// [0] [0] [0]
+// [0] [0] [0]
+// [0] [0] [0]
+
+int[,,] array3D = new int[3, 3, 3];
+// It is a square in 3D
+
+int[,] example2D = { { 1, 2 }, { 3, 4 } };
+// [1] [2] 
+// [3] [4] 
+
+Console.WriteLine(example2D[0, 0]);
+Console.WriteLine(example2D[0, 1]);
+Console.WriteLine(example2D[1, 0]);
+Console.WriteLine(example2D[1, 1]);
+
+string[,] example2DIndex =
+{
+    { "0,0", "0,1", "0,2"},
+    { "1,0", "1,1", "1,2"},
+    { "2,0", "2,1", "2,2"},
+};
+
+string[,,] example3DIndex =
+{
+    {
+      { "0,0,0", "0,0,1"},
+      { "0,1,0", "0,1,1"},
+    },
+    {
+      { "1,0,0", "1,0,1"},
+      { "1,1,0", "1,1,1"},
+    }
+};
+
+// Tic Tac Toe
+string[,] ticTacToeField =
+{
+    { "O", "X", "X"},
+    { "O", "O", "X"},
+    { "X", "X", "O"},
+};
+
+// Sum of the current row
+int[,] array = new int[,]
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+// Iterate over each row
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int rowSum = 0;
+    // Calculate the sum of the current row
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        rowSum += array[i, j];
+    }
+    // Print the sum of the current row to the console
+    Console.WriteLine(rowSum);
+}
+
+Console.ReadKey();
