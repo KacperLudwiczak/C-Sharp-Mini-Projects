6f9a5d4bdf078e15e46b351dfde593cc3c224460
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-06 15:02:43 +0100

    Add Old Style

---
+++ Old Style/Old Style/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Old_Style
+{
+    internal class Program
+    {
+        // This is a Filed (or instance variable)
+        static int myResult;
+
+        // Main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            // Validate user input
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                myResult = AddTwoValues(num, 10);
+                Console.WriteLine($"The result is {myResult}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int AddTwoValues(int value1, int value2)
+        {
+           myResult = value1 + value2;
+           return myResult;
+        }
+
+    }
+}
