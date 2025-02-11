a93536546563eaf87a5391ad5ea79d5ce2c78efe
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-11 13:58:37 +0100

    Add Delegates in C#

---
+++ Delegates in C#/Delegates in C#/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Delegates_in_C_
+{
+    internal class Program
+    {
+        // 1. Declaration:
+        public delegate void Notify(string message);
+        static void Main(string[] args)
+        {
+            // Delegates define a method signature, and any method assigned to a delegate must match this signature.
+
+            // 2. Instantiation:
+            Notify notifyDelegate = ShowMessage;
+            // Other version:
+            // Notify notifyDelegate = new Notify(notifyDelegate);
+
+            // 3. Invocation:
+            notifyDelegate("Hello, Delegates!");
+
+            Console.ReadKey();
+        }
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
